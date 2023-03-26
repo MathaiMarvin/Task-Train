a9ff4c6a2996af5cc3cf1670f7713ff343e2413d
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-27 00:48:34 +0300

    navigation landing page

---
+++ src/components/landing_page/NavigationLandingPage.js
@@ -0,0 +1,82 @@
+import React from "react";
+import '../../styles/LandingPage.css'
+import { Link } from "react-router-dom";
+const NavigationLandingPage = () => {
+    return ( 
+    
+        <div className="home-bar">
+            <div className="showcase-container">
+                <div className="showcase-projects">
+                    <div className="logo-show23">
+                        <div className="app-name">
+                            <div className="show-app">
+                                <button className="show-name1">
+                                    <Link to="/landing">
+                                        <div className="before-name">
+                                            
+                                            <h1 className="exact-app">
+                                                My Tasks
+                                            </h1>
+                                            
+                                        </div>
+                                    </Link>
+
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                        <div className="create-projects">
+                        <Link to="/create-project">
+                            <button className="create-button">
+                                <div className="div-button">
+                                     Create
+                                    
+                                </div>
+
+                            </button>
+                        </Link>
+
+                        </div>
+
+                    </div>
+
+                    <div className="my-navigation">
+                        <nav className="landing-nav">
+                            <ul className="landing-nav-list">
+                                <li className="nav-list1">
+                                    <a href="#" className="listed-item">
+                                        <div className="all-project-item">
+                                            All Tasks
+
+                                        </div>
+
+                                    </a>
+
+                                </li>
+                                {/* <li className="nav-list1">
+                                    <a href="#" className="listed-item">
+                                        <div className="all-project-item">
+                                            Create Project
+
+                                        </div>
+
+                                    </a>
+
+                                </li> */}
+
+                            </ul>
+
+                        </nav>
+
+                    </div>
+
+                </div>
+
+            </div>
+    </div>
+     );
+}
+ 
+export default NavigationLandingPage;
